aa05cf9384987c32101beab7459ca9f0cbf91a38
LeslieLaurent <leslie.laurent@edu.univ-fcomte.fr>
2025-11-12 15:10:14 +0100

    boutton clickable

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
-// *** CAROUSEL ***
-// TODO
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.querySelector(".carousel__container");
+  const prevButton = document.querySelector(".carousel__button--prev");
+  const nextButton = document.querySelector(".carousel__button--next");
+
+  const premierItem = document.querySelector(".carousel__item");
+  const scrollAmount = premierItem ? premierItem.clientWidth : 0;
+
+  if (carousel && prevButton && nextButton) {
+    prevButton.addEventListener("click", () => {
+      carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+
+    nextButton.addEventListener("click", () => {
+      carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
+    });
+  }
+});
